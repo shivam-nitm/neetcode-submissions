2de2da96a9a01f8f47500c7241bd4418c5635581
shivam-nitm <119390489+shivam-nitm@users.noreply.github.com>
2026-07-22 01:28:27 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        String lw = s.toLowerCase();    
+        for(int i =0; i< lw.length(); i++){
+            if(Character.isLetter(lw.charAt(i)) || Character.isDigit(lw.charAt(i))){
+                sb.append(lw.charAt(i));
+            }
+        }
+
+        String ss = sb.toString();
+        for(int i = 0, j = ss.length()-1; i< ss.length()/2 ; i++, j--){
+                if(ss.charAt(i) != ss.charAt(j)){
+                    return false;
+                }
+        }
+        return true;
+    }
+}
